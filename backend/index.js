79ee9ec079334c6d39979e95429ba1100927a295
VaibhavTalkhande <vaibhavtalkhande41@gmail.com>
2024-01-28 19:03:46 +0530

    paytm completed functionality

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,21 @@
 const express = require("express");
+const bodyParser = require('body-parser');
+const rootRouter =require('./routes/index');
+const cors = require('cors')
+const app = express();
+app.use(express.json());
+app.use(cors(
+    {
+        origin:"*"
+    }
+));
 
 
+app.use('/api/v1',rootRouter);
+
+
+app.listen(
+    3000,()=>{
+        console.log(`http://localhost:3000`);
+    }
+)
